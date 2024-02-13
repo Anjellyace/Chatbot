635b878144d50b5480511738771bbc810d744b7d
Anjellyace <angelicakristianne.pre1@gmail.com>
2024-02-13 14:42:29 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+while True:
+
+	user_input = input("Enter something: ").lower()
+
+	if user_input == 'h1':
+		print('Hello!')
+
+	elif user_input == 'good':
+		print('Good to hear!')
+
+	elif user_input=='bad':
+		print('WHY?!?!??!')
+
+	elif user_input=='because':
+		print('because what?')
+
+	elif user_input=='because':
+		print('...')
